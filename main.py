422d1bce685a2a5e9ace0a8daa032c21ff672310
mr-exception <darbandi1996@gmail.com>
2019-01-04 15:01:08 +0330

    updated

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
 import tweepy
 import json
-from utilities import clean_user_object
+import pymongo
+from time import time
+
+# my libraries
+from utilities import fetch_user, fetch_user_tweets
 
 CONSUMER_KEY = 'B2u1U1euSIqpJPpNSAL0Gty96'
 CONSUMER_SECRET = 'zGrPQdZNjdtzNAMLkWj60p3XLnbuVc37nepzE0vq5xW1AA7miU'
 
-screen_name = 'reza_binazar'
-
 # auth for tweeter
 auth = tweepy.AppAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-api = tweepy.API(auth)
+api = tweepy.API(auth, wait_on_rate_limit=True)
+print('cenncted to tweeter successfully!')
+# connect to mongodb
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["absenat"]
+print('connected to mongodb successfully!')
 
-# geting user informations
-user = api.get_user(screen_name)._json
-user = clean_user_object(user)
-print(json.dumps(user))
+users_collection = mydb["users"]
+tweets_collection = mydb["tweets"]
+
+def update_existing_users():
+  for user in users_collection.find({'last_user_fetch': {'$lt': time() - 3600}}, {'screen_name': True}).limit(100):
+    screen_name = user['screen_name']
+    fetch_user(screen_name, users_collection, api)
+
+def fetch_new_users(sreen_names):
+  for screen_name in sreen_names:
+    fetch_user(screen_name, users_collection, api)
+
+
+# fetch_new_users(['Rairhahs', 'amir_abbas', 'NaraghiAnita', 'alibordbari', 'arastuq'])
+# update_existing_users()
+fetch_user_tweets('reza_binazar', tweets_collection, users_collection, api)
--- utilities.py
+++ utilities.py
@@ -1,4 +1,7 @@
-def clean_user_object (user):
+from time import time
+import json
+
+def normilize_user_object (user):
   del user['id_str']
   del user['status']
   del user['statuses_count']
@@ -11,4 +14,69 @@ def clean_user_object (user):
   del user['notifications']
   del user['translator_type']
 
+  user['last_user_fetch'] = time()
+  
   return user
+
+def normilize_tweet_object (tweet, user):
+  del tweet['user']
+  del tweet['id_str']
+  del tweet['source']
+  del tweet['in_reply_to_status_id_str']
+  del tweet['in_reply_to_user_id_str']
+  del tweet['in_reply_to_status_id']
+  del tweet['in_reply_to_user_id']
+  
+  tweet['user_id'] = user['_id']
+  return tweet
+
+# geting user informations
+def fetch_user(screen_name, users_collection, api):
+  user = api.get_user(screen_name)._json
+  user = normilize_user_object(user)
+  
+  old_user = users_collection.find_one({'id': user['id']})
+  if old_user != None:
+    print('> updated user {}'.format(screen_name))
+    users_collection.update_one({'_id': old_user['_id']}, {'$set': user})
+  else:
+    print('+ got new user {}'.format(screen_name))
+    user['last_tweets_fetch']     = 0
+    user['last_followers_fetch']  = 0
+    user['last_freinds_fetch']    = 0
+    users_collection.insert_one(user)
+
+# fetch tweets
+def fetch_user_tweets(screen_name, tweets_collection, users_collection, api):
+  print('> checks {} in db'.format(screen_name))
+  user = users_collection.find_one({'screen_name': screen_name})
+  if user == None:
+    fetch_user(screen_name, users_collection, api)
+  else:
+    print('= users {} exists in db'.format(screen_name))
+  user = users_collection.find_one({'screen_name': screen_name})
+  
+  # calculating the offset id
+  tweet_id_offset = None
+  tweets_count = tweets_collection.count({'user_id': user['_id']})
+  if tweets_count >0:
+    last_tweets = tweets_collection.find({'user_id': user['_id']}, {'id': 1}).sort('_id', 1).limit(1)[0]
+    tweet_id_offset = last_tweets['id']
+    print('> offset updated: {}'.format(tweet_id_offset))
+  # print(last_tweets)
+  # return
+
+  max_tweets_fetch = 1000
+  tweets = []
+  for i in range(1, int(max_tweets_fetch/200)+1):
+    print('+ retriving page {} tweets of {}'.format(i, screen_name))
+    tweets_got = api.user_timeline(screen_name, count=200, page=i, since_id=tweet_id_offset)
+    for i,tweet in enumerate(tweets_got):
+      tweets_got[i] = normilize_tweet_object(tweet._json, user)
+    tweets += tweets_got
+    if len(tweets_got) < 200:
+      break
+  print('> got {} tweets'.format(len(tweets)))
+  if len(tweets) > 0:
+    tweets_collection.insert_many(tweets)
+  users_collection.update({'_id': user['_id']}, {'$set': {'last_tweets_fetch': time()}})
